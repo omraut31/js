58904a2655c758cfbc271ab1b2f06dfe417669de
omraut31 <129833710+omraut31@users.noreply.github.com>
2024-03-14 11:04:25 +0000

    operations

--- basic/4_conversionoperations.js
+++ basic/4_conversionoperations.js
@@ -1,18 +1,50 @@
 //we can use typeof(__)/typeof operations for check datatype
 let score= true
-console.log(typeof score)
+//console.log(typeof score)
 
 let intnumbervalue=Number(score)
-console.log(intnumbervalue) //shows NaN 
+//console.log(intnumbervalue) //shows NaN 
 
 // "42"=>42
 // "42abc"=> NaN
 // true =>1;false=>0
 
-let isloggedIn=""
-let booleanIsLoggedIn=Boolean(isloggedIn)
-console.log(booleanIsLoggedIn)
+// let isloggedIn=""
+// let booleanIsLoggedIn=Boolean(isloggedIn)
+// console.log(booleanIsLoggedIn)
 
 //1=> true;0=>false
 //" "=> false
 //"om"=> true
+
+
+/***************operations************************** */
+
+let value=3
+let negvalue=-value
+
+// console.log(2+2)
+// console.log(2-2)
+// console.log(2*2)
+// console.log(2**2)
+// console.log(2/2)
+// console.log(2%2)
+
+
+// let str1="hello "
+// let str2="om"
+// let str3=str1+str2;
+// console.log(str3)
+
+
+// console.log("1"+2)
+// console.log(1+"2")
+// console.log("1"+2+2)
+// console.log(1+"2"+"2"+1)
+
+// console.log(+true)
+// console.log(+false);
+
+let gamecounter=100
+gamecounter++
+console.log(gamecounter);
