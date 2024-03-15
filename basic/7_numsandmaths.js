c8d6a1e62953fb1379bb1cd8b0e4d927199f03cb
omraut31 <129833710+omraut31@users.noreply.github.com>
2024-03-15 10:58:42 +0000

    math and num

---
+++ basic/7_numsandmaths.js
@@ -0,0 +1,31 @@
+// const score=500
+
+// const balance= new Number(600)
+// console.log(balance);
+
+// console.log(balance.toString().length) //used to convert Number to string
+// console.log(balance.toFixed(2)) // to fixed number upto certain precision
+
+// const hundreds = 100000
+// console.log(hundreds.toLocaleString('en-IN')); // to make no. readable in form of commas
+
+
+//+++++++++++++++Maths+++++++++++=//
+
+// console.log(Math);
+// console.log(Math.abs(-3))
+// console.log(Math.round(3.8))
+// console.log(Math.ceil(8.3))
+// console.log(Math.floor(8.3))
+// console.log(Math.pow(2,3))
+// console.log(Math.sqrt(25))
+// console.log(Math.min(3,4,6)); and many more funtion
+
+
+console.log(Math.random())
+console.log((Math.random()*10)+1)
+
+const min = 10
+const max = 20
+
+console.log((Math.floor(Math.random() * (max - min + 1)) ) + min);
